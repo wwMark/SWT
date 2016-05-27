a5d30b21e2e72b4d4aea6a11813e2eccf7b21f98
wwMark <emailzummark@gmail.com>
2016-05-27 18:18:42 +0200

    App class is added, but it is not able to call the Main.main() method. Maybe there's something wrong with the module importation

---
+++ iMage/iMage.bundle/src/main/java/org/iMage/bundle/App.java
@@ -0,0 +1,12 @@
+package org.iMage.bundle;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("hello");
+		org.jis.Main.main(args);
+	}
+}
